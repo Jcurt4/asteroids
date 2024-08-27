2079517c1ddeb2d62a131d595fb5f10dadeb0b35
Jcurt4 <joe.curtis.iv@gmail.com>
2024-08-26 21:32:21 -0600

    C: start game loop

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import pygame 
+from constants import *
 
 def main():
-    print("Starting Asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True 
+
+    while running: 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
 
 
 
